0881d7edaf86e5e7c88ce980ebd0eb2b227dd743
Mr-Hasan-Raza <Mr.HasanRaza@otlook.com>
2024-03-04 01:47:29 +0500

    assignment-started

---
+++ app.js
@@ -0,0 +1,34 @@
+// QUESTION 01
+let que01 =[];
+console.log(que01);
+
+// QUESTION 02
+let que02 =[];
+console.log(que02);
+
+// QUESTION 03
+let que03 =['String array'];
+console.log(que03);
+
+// QUESTION 04
+let que04 =[4];
+console.log(que04);
+
+// QUESTION 05
+let que05 =[True];
+console.log(que05);
+
+// QUESTION 06
+let que06 =[6 , False , 'Mixed Array'];
+console.log(que06);
+
+// QUESTION 07
+var que07 =['Warid' , 'Jazz' , 'Telenor' , 'Ufone' , 'Mobilink' , 'Zong'];
+console.log(que07);
+
+// QUESTION 08
+var qualification = document.querySelector('ques08');
+var que08 =['SSC' , 'HSC' , 'BCS' , 'BS' , 'B.com' , 'MS' , 'M.' , 'Phil' , 'PhD'];
+qualification.innerHTML += (`<h1> ${qualification} </h1>`);
+
+// QUESTION 09
